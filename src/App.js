a9fc6ead1aa7f1eea74640844fa03c77a4be71bf
nungr2oo1 <nunganesh46@gmail.com>
2026-02-10 22:20:49 +0530

    Add console log to App component for rendering confirmation

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 
 function App() {
+
+  console.log("App component rendered");
+  
   return (
     <ThemeProvider>
       <Router>
